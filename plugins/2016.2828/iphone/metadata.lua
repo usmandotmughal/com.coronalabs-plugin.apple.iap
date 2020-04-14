220b5c584f776528d1555cf3649c573c39ec5806
Vlad Shcherban <vlad@coronalabs.com>
2020-04-13 23:13:11 -0400

    Adding binaries

---
+++ plugins/2016.2828/iphone/metadata.lua
@@ -0,0 +1,13 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+		staticLibs = { 'plugin_iap', },
+		frameworks = {},
+		frameworksOptional = {},
+		-- usesSwift = true,
+	},
+}
+
+return metadata
